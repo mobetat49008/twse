9bbc06738208a9ba4ee14a2a0c08812165c9f6b8
BreezeCat <jkjk010695@gmail.com>
2020-02-03 13:46:41 +0800

    0203 stock_list update

--- goodinfo.py
+++ goodinfo.py
@@ -2,6 +2,7 @@
 
 import requests
 from bs4 import BeautifulSoup
+from lxml import html
 
 
 headers = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.77 Safari/537.36' }
@@ -16,6 +17,16 @@ def Get_PreClose(stock_id):
     #PreClose = float(td_tags[4].string) #need Check
     PreClose = float(td_tags[0].string)
     return PreClose
+
+def Get_Shared(stock_id):
+    url = 'https://goodinfo.tw/StockInfo/BasicInfo.asp?STOCK_ID=' + stock_id
+    list_req = requests.post(url, headers = headers)
+    tree = html.fromstring(list_req.content)
+    data_set = tree.xpath('//td[@bgcolor="white"]/nobr/text()')
+    shared_raw_data = str(data_set[6])
+    Shared = int(shared_raw_data[:-2].replace(',',''))
+    return Shared
+    
 
 if __name__ == '__main__':
     C = Get_PreClose('9926')
