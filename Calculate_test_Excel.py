54448b1957ae93dd99194f17be54f3b629325e4d
BreezeCat <jkjk010695@gmail.com>
2020-05-17 18:29:52 +0800

    Get Price from ESUN excel file

---
+++ Calculate_test_Excel.py
@@ -0,0 +1,244 @@
+import ESUN_Interface as Pc
+import json
+import time
+import sched
+import Line_test
+import datetime
+from apscheduler.schedulers.background import BackgroundScheduler
+import goodinfo
+import threading
+from queue import Queue
+
+s = sched.scheduler(time.time, time.sleep)
+token = 'kNrW93XBFwlQmDizAm1TTHrWZsTG8ZTuaWqyrN3U9s5'
+#token = 'I5HvbkSz66CZ7RL3k2BkXmvMcNVLdib0J8fSPIvq3dx' #backup token
+token2 = 'Vr5QUop64kp7JXpTQdAyr2dqzrnyraREB5vsg3CCxUR'
+Today_file = 'PreIndex_tick_data/' + str(datetime.datetime.now().date())+'_tick_data.json'
+
+urlsplitlength = 160 #166
+stock_list=None 
+Weight=None 
+Index = 12001.01
+last_PreIndex = Index
+last_PreIndex=0
+
+def Load_Stock_List(list_file):
+
+    f = open(list_file , 'r' , encoding='utf8', newline='')
+    Stock_List = (f.read()).split()
+
+    return Stock_List
+
+def Get_Price(stock_list):
+    price = {}
+    Fail_list = []
+    for i in range(len(stock_list)//urlsplitlength +1):
+        small_stock_list = stock_list[i*urlsplitlength:min(len(stock_list),(i+1)*urlsplitlength)]
+        small_price, small_Fail = Pc.stock_price_crawler(small_stock_list)
+        price = {**price, **small_price}
+        Fail_list = Fail_list + small_Fail
+    
+    return price, Fail_list
+
+def Get_change(stock_list, req_item):
+    change = {}
+
+    result = Queue()
+    Pc.stock_change_crawler(stock_list, req_item, result)
+    change = result.get()
+    '''
+    for i in range(len(stock_list)//urlsplitlength +1):
+        small_stock_list = stock_list[i*urlsplitlength:min(len(stock_list),(i+1)*urlsplitlength)]
+        Pc.stock_change_crawler(small_stock_list, req_item, result)
+
+    for i in range(len(stock_list)//urlsplitlength +1):
+        small_change = result.get()
+        change = {**change, **small_change}
+    '''
+    return change
+
+def Load_shared(shared_file):
+    file = open(shared_file,'r',encoding = 'utf-8-sig')
+    shared = {}
+    line = file.readline()
+    while(line):
+        line = line.split(',')
+        shared[line[0]] = float(line[1])
+        line = file.readline()
+    file.close()
+    return shared
+
+def Calculate_Weight(price, shared):
+    market_value = {}
+    total_market_value = 0
+    for item in price:
+        if item in shared:
+            market_value[item] = price[item] * shared[item]
+            total_market_value = total_market_value + market_value[item]
+    weight = {}
+    for item in market_value:
+        weight[item] = market_value[item]*100/total_market_value
+    return weight
+    
+def Record_Json(Dict, save_name):
+    file = open(save_name,'w')
+    json.dump(Dict,file)
+    file.close()
+    return
+
+def Read_Json(file_name):
+    with open(file_name,"r", encoding='utf-8') as file:
+        data = json.load(file)
+        return data
+    
+def Record_Tick_Json(Dict, save_name):
+    file = open(save_name,'a+')
+    json.dump(Dict,file)
+    file.write('\n')
+    file.close()
+    return
+
+def Calculate_percent(change, weight):
+    percent = 0
+    for item in change:
+        if item in weight:
+            percent = percent + change[item]*weight[item]/100
+    return percent
+            
+def process(stock_list, weight, req_item):
+    global last_PreIndex
+    
+    if req_item == 'pz':
+        IND = '\n試搓指數[threading]:'
+    elif req_item == 'z':
+        IND = '\n現貨試算指數:'
+    else:
+        return 'Input Error'
+    print('Request...',datetime.datetime.now())
+    
+    Change = Get_change(stock_list, req_item)
+    P = Calculate_percent(Change, weight)
+    Premarket = Index*P/100
+    PreIndex = Index + Premarket   
+    
+    msg = IND + str('%.2f'%PreIndex) + '\n漲跌點數:' + str('%.2f'%Premarket)+'\n漲跌幅:'+ str('%.3f'%P) + '% \n資料時間：' + Change['Update_Time']
+    if abs(last_PreIndex - PreIndex)>1:        
+        t = threading.Thread(target=Line_test.lineNotifyMessage, args=(token, msg))
+        last_PreIndex = PreIndex
+    else:
+        t = threading.Thread(target=Line_test.lineNotifyMessage, args=(token2, msg))
+    t.start()
+        
+    tick_data = {}
+    tick_data['PreIndex'] = round(PreIndex, 2)
+    tick_data['Change'] = round(Premarket, 2)
+    tick_data['Time'] = Change['Update_Time']
+    Record_Tick_Json(tick_data, Today_file)
+
+        
+    return 'Finish'
+
+
+
+def EveryDay_Update():    
+    global stock_list
+    print('Update Start')
+    stock_list = Load_Stock_List('Stock_list.txt')
+    
+    stock_list.append('10000')
+    Price, Fail_list = Get_Price(stock_list)
+
+    for item in Fail_list:
+        Price[item] = goodinfo.Get_PreClose(item)
+        print(item, Price[item])
+        
+    Shared = Load_shared('shared.txt')
+    Weight = Calculate_Weight(Price,Shared)
+    Record_Json(Weight, 'Weight.json')
+    Index_dict = {}
+    Index_dict['Index'] = Price['TSE']
+    Index_dict['Time'] = str(datetime.datetime.now().date())
+    Record_Json(Index_dict, 'Index.json')
+    print('Update Finish')
+    
+    
+def Reload_parameter():
+    global stock_list, Weight, Index, last_PreIndex, Today_file
+    stock_list = Load_Stock_List('Stock_list.txt')
+    Weight = Read_Json('Weight.json')
+    Index_dict = Read_Json('Index.json')
+    Index = Index_dict['Index']
+    last_PreIndex = Index
+    Today_file = 'PreIndex_tick_data/' + str(datetime.datetime.now().date())+'_tick_data.json'
+    print('Reloaded')
+    
+def TWSE_Update():
+    twse_close = Pc.stock_price_crawler(['t00'])
+    Index_dict = {}
+    Index_dict['Index'] = twse_close [0]['t00']
+    Index_dict['Time'] = str(datetime.datetime.now().date())
+    Record_Json(Index_dict, 'Index.json')
+    
+    
+def Start_process():
+    now_time = datetime.datetime.now()
+    start_time = datetime.datetime.strptime(str(now_time.date())+'8:30', '%Y-%m-%d%H:%M')
+    end_time =  datetime.datetime.strptime(str(now_time.date())+'9:00', '%Y-%m-%d%H:%M')
+    if now_time >= start_time and now_time <= end_time:
+        req_item = 'pz'
+    else:
+        start_time = datetime.datetime.strptime(str(now_time.date())+'13:25', '%Y-%m-%d%H:%M')
+        end_time =  datetime.datetime.strptime(str(now_time.date())+'13:30', '%Y-%m-%d%H:%M')
+        if now_time >= start_time and now_time <= end_time:
+            req_item = 'pz'
+        else:
+            req_item = 'z' 
+
+    process(stock_list,Weight,req_item)
+    return
+
+EveryDay_Update_Flag = 0
+def EveryDay_Update_Flag_Change():
+    global EveryDay_Update_Flag
+    EveryDay_Update_Flag = 1
+    print('Update_Flag_UP')
+    return
+
+Process_Flag = 0
+def Process_start():
+    global Process_Flag
+    Process_Flag = 1
+    print('Process_Flag_UP')
+    return
+    
+def Process_stop():
+    global Process_Flag
+    Process_Flag = 0
+    print('Process_Flag_DOWN')
+    return
+    
+
+if __name__ == '__main__': 
+
+    Reload_parameter()
+
+    scheduler = BackgroundScheduler()
+    scheduler.add_job(Reload_parameter, trigger='cron', day_of_week='mon-fri', hour='08', minute="00", second="0",id='my_job_id',misfire_grace_time=30)
+    scheduler.add_job(EveryDay_Update_Flag_Change, trigger='cron', day_of_week='mon-fri', hour='21', minute="00", second="0",id='my_job_id_1',misfire_grace_time=30)
+    scheduler.add_job(Process_start, trigger='cron', day_of_week='mon-fri', hour='08', minute="35", second="0",id='my_job_id_2',misfire_grace_time=30)
+    scheduler.add_job(Process_start, trigger='cron', day_of_week='mon-fri', hour='13', minute="25", second="10",id='my_job_id_3',misfire_grace_time=30)
+    scheduler.add_job(Process_stop, trigger='cron', day_of_week='mon-fri', hour='09', minute="01", second="0",id='my_job_id_4',misfire_grace_time=30)
+    scheduler.add_job(Process_stop, trigger='cron', day_of_week='mon-fri', hour='13', minute="31", second="30",id='my_job_id_5',misfire_grace_time=30)
+
+    scheduler.start()
+   
+    while(1):
+        if Process_Flag == 1:
+            Start_process()
+        elif EveryDay_Update_Flag == 1:
+            EveryDay_Update()
+            EveryDay_Update_Flag = 0
+        else:
+            time.sleep(1)
+
+        
---
+++ ESUN_Interface.py
@@ -0,0 +1,82 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun May 17 14:19:05 2020
+
+@author: BreezeCat
+"""
+
+import xlwings as xw
+import datetime
+
+Item_Dict={'z':'F', 'y':'D', 'pz':'C', 'c':'A'}
+
+workbook = xw.Book('ESUN.xlsx')
+sheet = workbook.sheets['工作表1']
+
+def stock_price_crawler(targets):
+    data = {}
+    data['msgArray'] = []
+    for item in targets:
+        temp = {}
+        row = int(item)
+        temp['z'] = sheet.cells(row, Item_Dict['z']).value
+        temp['y'] = sheet.cells(row, Item_Dict['y']).value
+        temp['c'] = sheet.cells(row, Item_Dict['c']).value
+        data['msgArray'].append(temp)
+    # 紀錄更新時間
+    time = datetime.datetime.now()  
+    update_time = str(time.hour)+":"+str(time.minute)+":"+str(time.second)
+    price = {}
+    price['Update_Time'] = update_time 
+    Fail_list = []
+    for i in range(len(data['msgArray'])):
+        if 'z' in data['msgArray'][i]:
+            if data['msgArray'][i]['z'] != '--' and data['msgArray'][i]['z'] != None:
+                price[data['msgArray'][i]['c']] = float(data['msgArray'][i]['z'])
+            elif data['msgArray'][i]['y'] != '--' and data['msgArray'][i]['y'] != None:
+                price[data['msgArray'][i]['c']] = float(data['msgArray'][i]['y']) 
+    
+    return price, Fail_list
+
+def stock_change_crawler(targets, req_item, result): 
+    data = {}
+    data['msgArray'] = []
+    for item in targets:
+        temp = {}
+        row = int(item)
+        temp[req_item] = sheet.cells(row, Item_Dict[req_item]).value
+        temp['y'] = sheet.cells(row, Item_Dict['y']).value
+        temp['c'] = sheet.cells(row, Item_Dict['c']).value
+        data['msgArray'].append(temp)
+    
+     # 紀錄更新時間
+    time = datetime.datetime.now()  
+    update_time = str(time.hour)+":"+str(time.minute)+":"+str(time.second)
+   
+    change = {}
+    change['Update_Time'] = update_time 
+    for i in range(len(data['msgArray'])):
+        if req_item in data['msgArray'][i] and 'y' in data['msgArray'][i]:
+            if data['msgArray'][i][req_item] != None and data['msgArray'][i][req_item] != '--':
+                change[data['msgArray'][i]['c']] = (float(data['msgArray'][i][req_item])-float(data['msgArray'][i]['y']))*100/float(data['msgArray'][i]['y'])
+            
+    result.put(change)
+    return
+
+
+if __name__ == '__main__':        
+    # 欲爬取的股票代碼
+    stock_list = ['2002','2330','2317','3557','2884']
+    a, b = stock_price_crawler(stock_list)
+    print(a)
+'''
+for i in range(2,944):
+    num = sheet.cells(i,1).value
+    row = int(num)
+    sheet.cells(row,1).value = "=XQESUNAP|Quote!'"+num+".TW-ID'"
+    sheet.cells(row,2).value = "=XQESUNAP|Quote!'"+num+".TW-Name'"
+    sheet.cells(row,3).value = "=XQESUNAP|Quote!'"+num+".TW-SimulatePrice'"
+    sheet.cells(row,4).value = "=XQESUNAP|Quote!'"+num+".TW-PreClose'"
+    sheet.cells(row,5).value = "=XQESUNAP|Quote!'"+num+".TW-SimulateTime'"
+    sheet.cells(row,6).value = "=XQESUNAP|Quote!'"+num+".TW-Price'"
+'''
